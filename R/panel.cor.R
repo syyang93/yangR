9357c2a33150a338db7b6311c169b428b52e4009
Stephanie Yang <syang93@jhmi.edu>
2019-04-11 15:34:21 -0400

    missing data
    make panel.cor work for incomplete data

--- R/panel.cor.R
+++ R/panel.cor.R
@@ -14,7 +14,7 @@ panel.cor<-function(x,y,digits=2,prefix="",cex.cor,...)
 {
   usr<-par("usr");on.exit(par(usr))
   par(usr=c(0,1,0,1))
-  r<-cor(x,y)
+  r<-cor(x,y, use = 'complete.obs')
   txt<-format(c(r,0.123456789),digits=digits)[1]
   txt<-paste(prefix,txt,sep="")
   if(missing(cex.cor))cex.cor<-0.8/strwidth(txt)
