01903fbe50d3e73af24325f66b1c013bad8b6219
Stephanie Yang <syang93@jhmi.edu>
2019-04-22 13:28:15 -0400

    add find.outliers
    add function for removing outliers

---
+++ R/find.outliers.R
@@ -0,0 +1,19 @@
+#' Function that will find outliers in a vector (default: > 3 SD from mean), and return their indices
+#' 
+#' @param x A numeric vector
+#' 
+#' @export
+#' 
+#' @return The indices of the outliers
+#' 
+#' @examples
+#' find.outliers(mtcars$mpg)
+
+find.outliers <- function(x, sd = 3){
+  m <- mean(x)
+  s <- sd(x)
+  up.out <- which(x > (m + sd*s))
+  low.out <- which(x < (m - sd*s))
+  all.out <- union(up.out, low.out)
+  return(all.out)
+}
