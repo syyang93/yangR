27fcced87dfa573f2c862105b95440319c93e241
Stephanie Yang <syang93@jhmi.edu>
2019-05-30 11:31:01 -0400

    added quartile fxn

---
+++ R/make.quartiles.R
@@ -0,0 +1,20 @@
+#' Function to make quartiles from a column within a dataframe --> taken from fashaR
+#'
+#' @param test data that needs to be divided into quartiles
+#'
+#' @export
+#'
+#' @return output Dataframe with quartiles (categories and by number, 4 = highest quartile)
+#'
+#' @examples
+#' test2=makequartiles(test$resid.mtDNA)
+
+make_quartiles=function(test){
+  quartiles= cut(test, breaks=quantile(test, probs=seq(0,1,by=0.25), na.rm=T))
+  num.quartiles=quartiles
+  levels(num.quartiles)=c("1", '2', '3', '4')
+  num.quartiles=factor(as.character(num.quartiles))
+  test <- data.frame(quartiles = quartiles, num.quartiles = num.quartiles)
+  return(test)
+}
+
