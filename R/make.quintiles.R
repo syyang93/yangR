2aa97488b3f0e645394ff35df3c7eab95555fc0c
Stephanie Yang <syang93@jhmi.edu>
2019-05-30 17:07:24 -0400

    fix
    fix

--- R/make.quintiles.R
+++ R/make.quintiles.R
@@ -9,12 +9,12 @@
 #' @examples
 #' test2=makequintiles(test$resid.mtDNA)
 
-make_quintiles=function(test){
+make.quintiles=function(test){
   quintiles= cut(test, breaks=quantile(test, probs=seq(0,1,by=0.2), na.rm=T))
   num.quintiles=quintiles
   levels(num.quintiles)=c("1", '2', '3', '4', '5')
   num.quintiles=factor(as.character(num.quintiles))
   test <- data.frame(quintiles = quintiles, num.quintiles = num.quintiles)
-  return(test)
+  return(test$num.quintiles)
 }
 
