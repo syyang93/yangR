04965c78d3a9690b1bd156a1a8f99aaefc6a302e
Stephanie Yang <syang93@jhmi.edu>
2019-06-12 12:14:21 -0400

    fixed
    parenthesis in wrong place

--- R/make.quintiles.R
+++ R/make.quintiles.R
@@ -10,7 +10,7 @@
 #' test2=makequintiles(test$resid.mtDNA)
 
 make.quintiles=function(test){
-  quintiles= cut(test, breaks=quantile(test, probs=seq(0,1,by=0.2), na.rm=T, include.lowest = T))
+  quintiles= cut(test, breaks=quantile(test, probs=seq(0,1,by=0.2)), na.rm=T, include.lowest = T)
   num.quintiles=quintiles
   levels(num.quintiles)=c("1", '2', '3', '4', '5')
   num.quintiles=factor(as.character(num.quintiles))
